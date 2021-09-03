e9ae4afcaff8eff9808698160464f5b3aaa7d9e7
Vishal Nehra <vishalmeham2@gmail.com>
2021-09-04 05:00:07 +0530

    Add support to view sibling images in viewpager

--- app/src/main/java/com/amaze/fileutilities/image_viewer/ImageViewerAdapter.kt
+++ app/src/main/java/com/amaze/fileutilities/image_viewer/ImageViewerAdapter.kt
@@ -9,14 +9,14 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 class ImageViewerAdapter(
     fragmentManager: FragmentManager,
     lifecycle: Lifecycle,
-    private val imageModel: LocalImageModel)
+    private val imageModel: ArrayList<LocalImageModel>)
     : FragmentStateAdapter(fragmentManager, lifecycle) {
 
     override fun getItemCount(): Int {
-        return 1
+        return imageModel.size
     }
 
     override fun createFragment(position: Int): Fragment {
-        return ImageViewerFragment.newInstance(imageModel)
+        return ImageViewerFragment.newInstance(imageModel[position])
     }
 }
--- app/src/main/java/com/amaze/fileutilities/image_viewer/ImageViewerDialogActivity.kt
+++ app/src/main/java/com/amaze/fileutilities/image_viewer/ImageViewerDialogActivity.kt
@@ -1,5 +1,6 @@
 package com.amaze.fileutilities.image_viewer
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
