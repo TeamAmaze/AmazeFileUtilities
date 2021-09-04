da481e7eb383ce11ffdfe6851e3788cfc5720894
Vishal Nehra <vishalmeham2@gmail.com>
2021-09-05 05:15:57 +0530

    Add permission dialog support

--- app/src/main/java/com/amaze/fileutilities/LandingActivity.kt
+++ app/src/main/java/com/amaze/fileutilities/LandingActivity.kt
@@ -1,11 +1,11 @@
 package com.amaze.fileutilities
 
+import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
 import com.amaze.fileutilities.databinding.LandingActivityBinding
 
-class LandingActivity : AppCompatActivity() {
+class LandingActivity : PermissionActivity() {
 
     private lateinit var landingActivityBinding: LandingActivityBinding
 
@@ -13,5 +13,26 @@ class LandingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         landingActivityBinding = LandingActivityBinding.inflate(layoutInflater)
         setContentView(landingActivityBinding.root)
+        requestStorageTrigger()
+        landingActivityBinding.grantButton.setOnClickListener {
+            startExplicitPermissionActivity()
+        }
+    }
+
+    private fun requestStorageTrigger() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !checkStoragePermission()) {
+            requestStoragePermission(object: OnPermissionGrantedCallback{
+                override fun onPermissionGranted() {
+                    landingActivityBinding.notGrantedTextView.visibility = View.GONE
+                    landingActivityBinding.grantButton.visibility = View.GONE
+                }
+
+                override fun onPermissionNotGranted() {
+                    landingActivityBinding.notGrantedTextView.visibility = View.VISIBLE
+                    landingActivityBinding.grantButton.visibility = View.VISIBLE
+                }
+
+            }, true)
+        }
     }
 }
