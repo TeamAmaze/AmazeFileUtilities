ed4e29850a108c5a4cd2fe4072ddb9a2321dcd48
Vishal Nehra <vishalmeham2@gmail.com>
2021-11-21 19:04:14 +0530

    Minor fixes in video player

--- app/src/main/java/com/amaze/fileutilities/video_player/VideoPlayerDialogActivity.kt
+++ app/src/main/java/com/amaze/fileutilities/video_player/VideoPlayerDialogActivity.kt
@@ -1,20 +1,13 @@
 package com.amaze.fileutilities.video_player
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.*
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.os.bundleOf
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.amaze.fileutilities.PermissionActivity
 import com.amaze.fileutilities.R
-import com.amaze.fileutilities.databinding.GenericPagerViewerActivityBinding
 import com.amaze.fileutilities.databinding.VideoPlayerDialogActivityBinding
-import com.amaze.fileutilities.databinding.VideoPlayerFragmentBinding
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
 
 class VideoPlayerDialogActivity: PermissionActivity() {
 
@@ -40,32 +33,6 @@ class VideoPlayerDialogActivity: PermissionActivity() {
                 setReorderingAllowed(true)
                 add<VideoPlayerFragment>(R.id.fragment_container_view, args = bundle)
             }
-            viewBinding.fullScreenButton.setOnClickListener {
-                val intent = Intent(this, VideoPlayerActivity::class.java).apply {
-                    putExtra(VideoPlayerFragment.VIEW_TYPE_ARGUMENT, videoModel)
-                }
-                startActivity(intent)
-                this.finish()
-            }
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val menuInflater = menuInflater
-        menuInflater.inflate(R.menu.video_dialog, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            R.id.open_full -> {
-                val intent = Intent(this, VideoPlayerActivity::class.java).apply {
-                    putExtra(VideoPlayerFragment.VIEW_TYPE_ARGUMENT, videoModel)
-                }
-                startActivity(intent)
-                this.finish()
-            }
-        }
-        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/amaze/fileutilities/video_player/VideoPlayerFragmentViewModel.kt
+++ app/src/main/java/com/amaze/fileutilities/video_player/VideoPlayerFragmentViewModel.kt
@@ -1,10 +1,14 @@
 package com.amaze.fileutilities.video_player
 
 import androidx.lifecycle.ViewModel
+import com.google.android.exoplayer2.MediaItem
 
 class VideoPlayerFragmentViewModel: ViewModel() {
 
     var playWhenReady = false
     var currentWindow = 0
     var playbackPosition = 0L
+    var videoModel: LocalVideoModel? = null
+    var fullscreen = false
+    var fitToScreen = false
 }
