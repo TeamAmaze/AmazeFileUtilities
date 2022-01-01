c75df08a4e3153b0920d1507770a65ae9d668872
Vishal Nehra <vishalmeham2@gmail.com>
2022-01-01 18:58:24 +0530

    Add about activity

---
+++ app/src/main/java/com/amaze/fileutilities/utilis/Utils.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2021-2022 Team Amaze - Arpit Khurana <arpitkh96@gmail.com>, Vishal Nehra <vishalmeham2@gmail.com>,
+ * Emmanuel Messulam<emmanuelbendavid@gmail.com>, Raymond Lai <airwave209gt at gmail.com>. All Rights reserved.
+ *
+ * This file is part of Amaze File Utilities.
+ *
+ * 'Amaze File Utilities' is a registered trademark of Team Amaze. All other product
+ * and company names mentioned are trademarks or registered trademarks of their respective owners.
+ */
+
+package com.amaze.fileutilities.utilis
+
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+
+class Utils {
+
+    companion object {
+
+        const val URL_PRIVACY_POLICY = "https://www.teamamaze.xyz/privacy-policy"
+
+        /**
+         * Open url in browser
+         *
+         * @param url given url
+         */
+        fun openURL(url: String?, context: Context) {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            try {
+                context.startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
