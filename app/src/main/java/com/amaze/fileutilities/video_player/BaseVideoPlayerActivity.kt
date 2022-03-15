af76839f3ed18b74fa0181c6952fcc9a40832df0
Vishal Nehra <vishalmeham2@gmail.com>
2022-03-15 05:45:54 +0530

    Implement video player gestures

--- app/src/main/java/com/amaze/fileutilities/video_player/BaseVideoPlayerActivity.kt
+++ app/src/main/java/com/amaze/fileutilities/video_player/BaseVideoPlayerActivity.kt
@@ -11,14 +11,19 @@
 package com.amaze.fileutilities.video_player
 
 import android.app.PictureInPictureParams
+import android.content.Context
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import android.content.res.Configuration
+import android.graphics.Point
+import android.media.AudioManager
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.media.session.MediaSessionCompat
 import android.util.Rational
+import android.view.Display
+import android.view.MotionEvent
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
@@ -30,16 +35,30 @@ import androidx.lifecycle.ViewModelProvider
 import com.amaze.fileutilities.PermissionsActivity
 import com.amaze.fileutilities.R
 import com.amaze.fileutilities.databinding.VideoPlayerActivityBinding
+import com.amaze.fileutilities.utilis.hideFade
+import com.amaze.fileutilities.utilis.showFade
 import com.amaze.fileutilities.utilis.showToastInCenter
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
 
-abstract class BaseVideoPlayerActivity : PermissionsActivity() {
+abstract class BaseVideoPlayerActivity : PermissionsActivity(), View.OnTouchListener {
 
-//    abstract fun getVideoPlayBinding(): VideoPlayerActivityBinding
-//    abstract fun getPlayer(): ExoPlayer?
-//    abstract fun getViewModel(): VideoPlayerActivityViewModel?
+    private var intLeft = false
+    private var intRight = false
+    private var sWidth = 0
+    private var sHeight = 0
+    private var diffX = 0L
+    private var diffY = 0L
+    private var downX = 0f
+    private var downY = 0f
+    private var currentBrightness = 0.5f
+    private var size: Point? = null
+    private var deviceDisplay: Display? = null
+    private var gestureSkipStepMs: Int = 200
+    private val gestureThreshold = 75
+
+    private var mAudioManager: AudioManager? = null
 
     abstract fun getVideoModel(): LocalVideoModel?
 
@@ -61,6 +80,12 @@ abstract class BaseVideoPlayerActivity : PermissionsActivity() {
         initMediaItem()
         initializePlayer()
         refactorPlayerController(!(videoPlayerViewModel?.isInPictureInPicture ?: false))
+        mAudioManager =
+            applicationContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        getScreenSize()
+        if (!isDialogActivity()) {
+            viewBinding.videoView.setOnTouchListener(this)
+        }
     }
 
     override fun onPause() {
@@ -73,9 +98,17 @@ abstract class BaseVideoPlayerActivity : PermissionsActivity() {
         initializePlayer()
     }*/
 
-    override fun onStop() {
-        super.onStop()
+    override fun onDestroy() {
+        super.onDestroy()
         releasePlayer()
+    }
+
+    private fun getScreenSize() {
+        deviceDisplay = windowManager.defaultDisplay
+        size = Point()
+        deviceDisplay?.getSize(size)
+        sWidth = size!!.x
+        sHeight = size!!.y
     }
 
     private fun initMediaItem() {
@@ -293,5 +326,98 @@ abstract class BaseVideoPlayerActivity : PermissionsActivity() {
                 this.enterPictureInPictureMode()
             }
         }
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        when (event!!.action) {
+            MotionEvent.ACTION_DOWN -> {
+                // touch is start
+                downX = event.x
+                downY = event.y
+                if (event.x < sWidth / 2) {
+
+                    // here check touch is screen left side
+                    intLeft = true
+                    intRight = false
+                    viewBinding.brightnessHintParent.showFade(300)
+                } else if (event.x > sWidth / 2) {
+
+                    // here check touch is screen right side
+                    intLeft = false
+                    intRight = true
+                    viewBinding.volumeHintParent.showFade(300)
+                }
+
+                if (gestureSkipStepMs == 200) {
+                    gestureSkipStepMs = ((player?.duration!! * 5.0) / sWidth).toInt()
+                }
+            }
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_MOVE -> {
+
+                // finger move to screen
+                val x2 = event.x
+                val y2 = event.y
+                diffX = Math.ceil((event.x - downX).toDouble()).toLong()
+                diffY = Math.ceil((event.y - downY).toDouble()).toLong()
+                if (Math.abs(diffY) > Math.abs(diffX) && Math.abs(diffY) > gestureThreshold) {
+                    if (intLeft) {
+                        // if left its for brightness
+                        invalidateBrightness(downY > y2)
+                    } else if (intRight) {
+                        // if right its for audio
+                        invalidateVolume(downY > y2)
+                    }
+                } else if (Math.abs(diffY) < Math.abs(diffX)) {
+                    player?.currentPosition?.let {
+                        if (downX < x2) {
+                            player?.seekTo(it + gestureSkipStepMs)
+                        } else {
+                            player?.seekTo(it - gestureSkipStepMs)
+                        }
+                    }
+                }
+
+                if (event.action == MotionEvent.ACTION_UP) {
+                    viewBinding.volumeHintParent.hideFade(300)
+                    viewBinding.brightnessHintParent.hideFade(300)
+                }
+            }
+        }
+        return true
+    }
+
+    private fun invalidateBrightness(doIncrease: Boolean) {
+        if (doIncrease) {
+            if (currentBrightness <= 0.993f) {
+                currentBrightness += 0.007f
+            }
+        } else {
+            if (currentBrightness >= 0.007f) {
+                currentBrightness -= 0.007f
+            }
+        }
+        val layout = window.attributes
+        layout.screenBrightness = currentBrightness
+        window.attributes = layout
+        viewBinding.brightnessProgress.setMax(100)
+        viewBinding.brightnessProgress.setProgress((currentBrightness * 100).toInt())
+    }
+
+    private fun invalidateVolume(doIncrease: Boolean) {
+        if (doIncrease) {
+            mAudioManager!!.adjustVolume(
+                AudioManager.ADJUST_RAISE,
+                AudioManager.FLAG_PLAY_SOUND
+            )
+        } else {
+            mAudioManager!!.adjustVolume(
+                AudioManager.ADJUST_LOWER,
+                AudioManager.FLAG_PLAY_SOUND
+            )
+        }
+        val currentVolume: Int = mAudioManager!!.getStreamVolume(AudioManager.STREAM_MUSIC)
+        val maxVolume: Int = mAudioManager!!.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        viewBinding.volumeProgress.setMax(maxVolume)
+        viewBinding.volumeProgress.setProgress(currentVolume)
     }
 }
