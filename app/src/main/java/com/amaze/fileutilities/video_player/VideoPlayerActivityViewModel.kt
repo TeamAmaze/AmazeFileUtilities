2282c45ba22fcfb02ad6e54bc380301440e255d2
Vishal Nehra <vishalmeham2@gmail.com>
2022-03-19 04:27:13 +0530

    Implement fetch subtitles online; minor fixes when brightness and volume hints are visible

--- app/src/main/java/com/amaze/fileutilities/video_player/VideoPlayerActivityViewModel.kt
+++ app/src/main/java/com/amaze/fileutilities/video_player/VideoPlayerActivityViewModel.kt
@@ -10,7 +10,16 @@
 
 package com.amaze.fileutilities.video_player
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import org.jsoup.Jsoup
+import retrofit2.Retrofit
+import java.util.*
+import kotlin.collections.ArrayList
 
 class VideoPlayerActivityViewModel : ViewModel() {
 
@@ -26,4 +35,86 @@ class VideoPlayerActivityViewModel : ViewModel() {
     var isSubtitleAvailable = false
     var isSubtitleEnabled = false
     var subtitleFilePath: String? = null
+
+    fun getSubtitlesAvailableLanguages(): LiveData<List<LanguageSelectionAdapter
+            .SubtitleLanguageAndCode>?> {
+        return liveData(context = viewModelScope.coroutineContext + Dispatchers.Default) {
+            emit(null)
+            val retrofit = Retrofit.Builder()
+                .baseUrl(SubtitlesApi.OPEN_SUBTITLES_BASE)
+                .build()
+            val service = retrofit.create(SubtitlesApi::class.java)
+            service.searchSubsConfigs()?.execute()?.let {
+                response ->
+                if (response.body() != null) {
+                    val document = Jsoup.parse(response.body()!!.string())
+                    val optionTags = document.select("select[name=SubLanguageID]")
+                        .select("option")
+                    var languageCodes = optionTags.eachAttr("value")
+                    var languageValues = optionTags.eachText()
+                    if (languageCodes.isEmpty() || languageCodes.isEmpty()) {
+                        languageCodes = Collections.singletonList("all")
+                        languageValues = Collections.singletonList("ALL")
+                    }
+                    try {
+                        val languageAndCodeList = ArrayList<LanguageSelectionAdapter
+                            .SubtitleLanguageAndCode>()
+                        languageAndCodeList.add(
+                            LanguageSelectionAdapter
+                                .SubtitleLanguageAndCode("Languages", "all")
+                        )
+                        for (i in languageCodes.indices) {
+                            languageAndCodeList
+                                .add(
+                                    LanguageSelectionAdapter
+                                        .SubtitleLanguageAndCode(
+                                            languageValues[i],
+                                            languageCodes[i]
+                                        )
+                                )
+                        }
+                        emit(languageAndCodeList)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        emit(
+                            Collections.singletonList(
+                                LanguageSelectionAdapter
+                                    .SubtitleLanguageAndCode("ALl", "all")
+                            )
+                        )
+                    }
+                } else {
+                    emit(null)
+                }
+            }
+        }
+    }
+
+    fun getSubtitlesList(
+        languageList: List<LanguageSelectionAdapter.SubtitleLanguageAndCode>,
+        movieName: String
+    ): LiveData<List<LanguageSelectionAdapter
+            .SubtitleLanguageAndCode>?> {
+        return liveData(context = viewModelScope.coroutineContext + Dispatchers.Default) {
+            emit(null)
+            val retrofit = Retrofit.Builder()
+                .baseUrl(SubtitlesApi.OPEN_SUBTITLES_BASE)
+                .build()
+            val service = retrofit.create(SubtitlesApi::class.java)
+            service.postSearchQuery(
+                languageList.map { it.code }.filter { it.isNotEmpty() }
+                    .joinToString(","),
+                movieName
+            )?.execute()?.let {
+                response ->
+                Log.i(
+                    javaClass.simpleName,
+                    "Subtitles list doc " +
+                        response.body()!!.string()
+                )
+                val document = Jsoup.parse(response.body().toString())
+                emit(null)
+            }
+        }
+    }
 }
