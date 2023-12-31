0ca807a3ff73590b180291849d0bfeacf00b72cf
Vishal Nehra <vishalmeham2@gmail.com>
2023-12-31 19:01:16 +0530

    Update licenses for new year

--- app/src/main/java/com/amaze/fileutilities/home_page/ui/files/FilesViewModel.kt
+++ app/src/main/java/com/amaze/fileutilities/home_page/ui/files/FilesViewModel.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2023 Arpit Khurana <arpitkh96@gmail.com>, Vishal Nehra <vishalmeham2@gmail.com>,
+ * Copyright (C) 2021-2024 Arpit Khurana <arpitkh96@gmail.com>, Vishal Nehra <vishalmeham2@gmail.com>,
  * Emmanuel Messulam<emmanuelbendavid@gmail.com>, Raymond Lai <airwave209gt at gmail.com> and Contributors.
  *
  * This file is part of Amaze File Utilities.
@@ -1961,7 +1961,7 @@ class FilesViewModel(val applicationContext: Application) :
             recentlyUpdatedAppsLiveData?.postValue(ArrayList(result.reversed()))
         }
     }
-    
+
     fun getLargeSizeDiffApps(): LiveData<MutableList<MediaFileInfo>?> {
         if (largeSizeDiffAppsLiveData == null) {
             largeSizeDiffAppsLiveData = MutableLiveData()
